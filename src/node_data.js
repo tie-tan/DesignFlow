588d87e138cfb39974bd782a5cc2ae95ea7bb19b
NH1934 <sairaviteja.aitha@braneenterprises.com>
2024-03-08 14:23:04 +0530

    nodedata

--- src/node_data.js
+++ src/node_data.js
@@ -2,11 +2,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faEllipsisV, faPerson } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';  
 import { Handle,Position,NodeProps,useStore} from 'reactflow';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Collapsible from 'react-collapsible';
  
 export function Node({id,data,position}) {
+  const wrapperRef = useRef(null);
+ // const collapsibleRef = useRef(null);
+  const [collapsiblePosition, setCollapsiblePosition] = useState({});
   const [displayName, setInputValue] = useState(`${data.displayName}`);
+
+  const handleClickCollapsible = () => {
+    if (wrapperRef.current) {
+      const position = wrapperRef.current.getBoundingClientRect();
+      console.log('Collapsible position:', position);
+      // You can use `position` as needed
+    }
+  };
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -59,7 +70,7 @@ export function Node({id,data,position}) {
         <div style={{color:iconColor,width:20,fontSize:40,marginLeft:8,marginTop:13,marginRight:8}}><FontAwesomeIcon  icon={icon} /></div>
         <div style = {{width:320,textAlign:'center',alignItems:'center',display:'flex',justifyContent:'center'}}>{data.displayName}</ div>
       </div> 
-      :<div style={{height:304,width:254,border:'1px solid black', borderRadius:4,display:'flex',flexDirection:'column'}}>
+      :<div ref = {wrapperRef} style={{height:304,width:254,border:'1px solid black', borderRadius:4,display:'flex',flexDirection:'column'}}>
       <div style={{height:40,backgroundColor:'#f5f5f5',paddingLeft:16,borderBottom:'1px solid #D0D0D0',display:'flex',flexDirection:'row'}}>
         <div style={{height:10,width:10,transform:'rotate(45deg)',backgroundColor:'#808080',marginRight:10,marginTop:14}}></div>
         <div style={{fontFamily:'Inter',fontSize:12,marginTop:12}}>{data.name}</div>
@@ -68,7 +79,7 @@ export function Node({id,data,position}) {
       <div style={{backgroundColor:'white',height:56}}>
         <div style={{margin:16,backgroundColor:'white'}}>
           <div style={{fontSize:11,color:'#808080'}}>Display Name</div>
-          <div onClick={StopPropagation}><input style={{borderColor:'#d8dce0',borderWidth:0.5,borderRadius:2}} type='text' value={displayName} onChange={handleChange} placeholder='Enter Name' /></div>
+          <div ><input style={{borderColor:'#d8dce0',borderWidth:0.5,borderRadius:2}} type='text' value={displayName} onChange={handleChange} placeholder='Enter Name' /></div>
         </div>
       </div>
       <div style={{backgroundColor:'white',height:56}}>
@@ -79,7 +90,7 @@ export function Node({id,data,position}) {
       </div>
       <div >
           {data.layers.map((item) => (
-            <div style={{
+            <div onClick={StopPropagation} style={{
               backgroundColor: '#f9f9f9',
               color: '#333',
               border: '1px solid #ccc',
@@ -88,8 +99,8 @@ export function Node({id,data,position}) {
               padding: '10px',
               cursor: 'pointer',
               alignItems: 'center',
-            }} onClick={StopPropagation}>
-              <Collapsible trigger={item.type}>
+            }} >
+              <Collapsible trigger={item.type} onOpening={handleClickCollapsible} >
                 {item.participatingItems.map((x) => (
                   <div style={{
                     backgroundColor: '#dcd8dc',
@@ -101,7 +112,7 @@ export function Node({id,data,position}) {
                     cursor: 'pointer',
                     alignItems: 'center',
                   }}>
-                    <Collapsible trigger={x.id} >
+                    <Collapsible trigger={x.id}  >
                       {x.item.DATA.nslAttributes.map((y) => (
                         <p style={{
                           backgroundColor: '#f9f9f8',
